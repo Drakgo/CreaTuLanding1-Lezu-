e7bd0bb001d08e0598fef71bfaaae0c41d2992f2
Drakgo <46280829+Drakgo@users.noreply.github.com>
2025-07-30 13:55:40 -0300

    entrega

--- components/CartWidget.js
+++ components/CartWidget.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CartWidget = () => {
   return (
     <div>
-    <span>3</span>
+    <span> </span>
     </div>
   );
 };
--- components/ItemListConteiner.js
+++ components/ItemListConteiner.js
@@ -4,7 +4,7 @@ const ItemListContainer = ({ greeting }) => {
   return (
     <section style={{ padding: '2rem' }}>
       <h2>{greeting}</h2>
-      {/* Aquí irá el catálogo de productos en futuras entregas */}
+      {/* Catálogo */}
     </section>
   );
 };
